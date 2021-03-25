8a0e3f4708cb5691dfd3bf89f871ded2bc88993e
Saransh Chauhan <saranshchauhan700@gmail.com>
2021-03-25 19:02:36 +0530

    file updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-
+import 'input_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,32 +13,10 @@ class BMICalc extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: InputPage(),
       theme: ThemeData.dark().copyWith(
-          primaryColor: Color(0xFF0A0E21), // remove # with 0xff
-          accentColor: Colors.redAccent,
-          scaffoldBackgroundColor: Color(0xFF0A0E21),
-          //textTheme: TextTheme(bodyText2: TextStyle(color: Color(0xffffffff)))),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calc'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.add),
+        primaryColor: Color(0xFF0A0E21), // remove # with 0xff
+        accentColor: Colors.redAccent,
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        //textTheme: TextTheme(bodyText2: TextStyle(color: Color(0xffffffff)))
       ),
     );
   }
