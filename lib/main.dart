de92c4544b41040b52390f4705d14726fc246526
Saransh Chauhan <saranshchauhan700@gmail.com>
2021-03-27 17:44:45 +0530

    update color

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class BMICalc extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21), // remove # with 0xff
+        primaryColor: Color(0xFF0A0E21), // replaced # with 0xff
         accentColor: Colors.redAccent,
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         //textTheme: TextTheme(bodyText2: TextStyle(color: Color(0xffffffff)))
