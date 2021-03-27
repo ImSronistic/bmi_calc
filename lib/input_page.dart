272cdf22b0c9db54a352ebb9db967ae842977633
Saransh Chauhan <saranshchauhan700@gmail.com>
2021-03-27 17:44:21 +0530

    extracted Widget i.e ReusableCard & IconContent

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const activeColor = Color(0xFF0A0E33);
 
 class InputPage extends StatefulWidget {
   @override
@@ -16,35 +19,90 @@ class _InputPageState extends State<InputPage> {
         children: [
           Expanded(
               child: Row(
-            children: [
-              Expanded(child: ReusableCard()),
-              Expanded(child: ReusableCard())
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(
+                  colour: activeColor,
+                  cardChild: IconContent(),
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  colour: activeColor,
+                ),
+              )
             ],
           )),
-          Expanded(child: ReusableCard()),
+          Expanded(
+              child: ReusableCard(
+            colour: activeColor,
+          )),
           Expanded(
               child: Row(
             children: [
-              Expanded(child: ReusableCard()),
-              Expanded(child: ReusableCard())
+              Expanded(
+                child: ReusableCard(
+                  colour: activeColor,
+                ),
+              ),
+              Expanded(
+                  child: ReusableCard(
+                colour: activeColor,
+              ))
             ],
           )),
+          Container(
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: 50.0,
+            color: Colors.pink,
+          )
         ],
       ),
     );
   }
 }
 
-class ReusableCard extends StatelessWidget {
-  const ReusableCard({Key key}) : super(key: key);
+class IconContent extends StatelessWidget {
+  IconContent({@required this.icon, @required this.text});
+  final Widget icon;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          FontAwesomeIcons.mars,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          'MALE',
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xff8d8e98),
+          ),
+        )
+      ],
+    );
+  }
+}
 
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour, this.cardChild});
+  final Color colour;
+  final Widget cardChild;
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
-        color: Color(0xFF0A0E33),
+        color: colour,
       ),
     );
   }
