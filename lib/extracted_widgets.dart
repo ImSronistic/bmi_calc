a42a2fe6228bfa9cc6d667f80903e14bacee9d43
Saransh Chauhan <saranshchauhan700@gmail.com>
2021-04-01 23:14:19 +0530

    add function as first order Object

--- lib/extracted_widgets.dart
+++ lib/extracted_widgets.dart
@@ -31,17 +31,22 @@ class IconContent extends StatelessWidget {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour, this.cardChild});
+  ReusableCard({@required this.colour, this.cardChild, this.gestureDetector});
   final Color colour;
   final Widget cardChild;
+  final Function gestureDetector;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: colour,
+    return GestureDetector(
+      onTap: gestureDetector,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: colour,
+        ),
       ),
     );
   }
